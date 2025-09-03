ce641b0a774c3d7cfc14d2fa2c0c0b3cac6f946f
scespificio <steve.costalat@espificio.com>
2025-09-03 14:02:25 +0200

    Update creation_EC2.sh

--- creation_EC2.sh
+++ creation_EC2.sh
@@ -2,8 +2,8 @@
 
 # === CONFIGURATION ===
 REGION="eu-west-3"
-KEY_PATH="/home/$(whoami)/Emilie_key.pem"    # Chemin local vers la clé privée
-KEY_NAME="Emilie_key"                        # Nom de la paire de clés AWS
+KEY_PATH="~/.ssh/scespificio.pem"    # Chemin local vers la clé privée
+KEY_NAME="scespificio"                        # Nom de la paire de clés AWS
 AMI="ami-0160e8d70ebc43ee1"                  # Ubuntu 22.04 LTS - Paris
 INSTANCE_TYPE="t2.small"
 USERNAME="ubuntu"
